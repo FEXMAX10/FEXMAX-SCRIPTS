9dd0fddeef368fc0aecfba8b1577761fb5ecbb43
FEXMAX10 <felixjuliangg@gmail.com>
2026-07-31 19:35:05 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cine-fexmax.ddns.net:367/api/public/dl/rvJbAr0X?inline=true"))()
+loadstring(game:HttpGet("http://cine-fexmax.ddns.net:367/api/public/dl/rvJbAr0X?inline=true"))()
